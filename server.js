e15a3ef6ecdbef81ece0da106316a2a9c461253b
venky27-cs <shashank.ks.aais@gmail.com>
2026-05-17 11:16:12 +0530

    Add files via upload
    Modified server.js and package.json to enable cloud hosting

--- server.js
+++ server.js
@@ -356,7 +356,7 @@ function tryMatchmaking(timeControl) {
     }
 }
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
